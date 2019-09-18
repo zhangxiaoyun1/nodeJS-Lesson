f29f333232001b20e59de97c0d3361f8f024d853
zhangxiaoyun1 <2210545262@qq.com>
2019-09-18 15:51:04 +0800

    message

--- helloWoeldServer.js
+++
@@ -1,9 +0,0 @@
-const http=require("http");
-
-var serve=http.createServer(function(req,res){
-    res.write("Hello world!");
-    res.end();
-});
-
-serve.listen(8080);
-console.log("serve is listening 8080");
